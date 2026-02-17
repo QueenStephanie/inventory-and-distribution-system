31c352e64768cbfaede759f7df4cb68226b02426
Queen Stephanie <queenstephanie.betonio@nmsc.edu.ph>
2026-02-17 21:02:28 +0800

    Add 'Manage Materials' link to superadmin dashboard and related pages

--- superadmin/manage_branches.php
+++ superadmin/manage_branches.php
@@ -107,6 +107,14 @@ include '../includes/header.php';
     </a>
 </li>
 <li class="menu-item">
+    <a href="manage_materials.php">
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M18 2H2C0.9 2 0 2.9 0 4v12c0 1.1 0.9 2 2 2h16c1.1 0 2-0.9 2-2V4c0-1.1-0.9-2-2-2zm-1 13H3V5h14v10z"/>
+        </svg>
+        Manage Materials
+    </a>
+</li>
+<li class="menu-item">
     <a href="system_reports.php">
         <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
             <path d="M4 2h12c1.1 0 2 0.9 2 2v12c0 1.1-0.9 2-2 2H4c-1.1 0-2-0.9-2-2V4c0-1.1 0.9-2 2-2zm0 14h4V8H4v8zm6 0h4v-6h-4v6zm6 0h4v-4h-4v4z"/>
